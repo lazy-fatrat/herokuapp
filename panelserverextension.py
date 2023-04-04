5def0f2135ae7da70d7f9628ca90d326b09806f5
lazy-fatrat <57050692+lazy-fatrat@users.noreply.github.com>
2023-04-04 09:52:38 +0200

    Update panelserverextension.py

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the App.py directory with bokeh server"""
-    Popen(["panel", "serve", "App.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "App_v2_RW_Cv2.py", "--allow-websocket-origin=*"])
